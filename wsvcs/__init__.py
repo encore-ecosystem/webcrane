8baecf8846922b1533c2cad020dfdf61eb15dde2
Максим <m.e.shushkevich@yandex.com>
2024-09-12 18:28:30 +0300

    First boost checkpoint (find near-optimal value)

--- wsvcs/__init__.py
+++ wsvcs/__init__.py
@@ -26,5 +26,12 @@ valid_commands = ['init', 'deploy', 'pull', 'push', 'client', 'run']
 # Package Max Size
 # ======================
 
-#                   mb   bytes
+#                   mb    bytes
 PACKAGE_MAX_SIZE = 512 * 1024
+
+
+#  256 * 1024   - 10.35
+#  384 * 1024   -  8.22
+#  512 * 1024   -  8.02
+#  768 * 1024   -  9.22
+# 1024 * 1024 - too big
